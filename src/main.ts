a1a7efc38dc0f9616bad6701b70e9118f8368c27
Jhon Lay Lopez <132878493+Lopez-JhonLay@users.noreply.github.com>
2025-03-25 11:19:57 +0800

    import dependecies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createPinia } from "pinia";
+
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+import { useAuthStore } from "./stores/authStore";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(ElementPlus);
+
+const authStore = useAuthStore();
+authStore.initializeAuth();
+
+app.mount("#app");
