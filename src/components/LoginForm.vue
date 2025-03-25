cfa7eaf0a916ed9dbb6ed71a076c193f16acafaf
Jhon Lay Lopez <132878493+Lopez-JhonLay@users.noreply.github.com>
2025-03-25 11:22:18 +0800

    create login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+
+import type { FormInstance, FormRules } from "element-plus";
+import { Lock, User, Loading } from "@element-plus/icons-vue";
+
+import { useAuthStore } from "../stores/authStore";
+
+const useAuth = useAuthStore();
+
+const { login } = useAuth;
+
+const ruleFormRef = ref<FormInstance>();
+
+const validateUsername = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the username"));
+  } else {
+    callback();
+  }
+};
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the password"));
+  } else {
+    callback();
+  }
+};
+
+const ruleForm = reactive({
+  username: "",
+  password: "",
+});
+
+const rules = reactive<FormRules<typeof ruleForm>>({
+  username: [{ validator: validateUsername, trigger: "blur" }],
+  password: [{ validator: validatePass, trigger: "blur" }],
+});
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("submit! valid sya");
+      console.log(login(ruleForm));
+    } else {
+      console.log("error submit!");
+    }
+  });
+};
+
+const isLoading = ref(false);
+</script>
+
+<template>
+  <el-form
+    ref="ruleFormRef"
+    style="max-width: 400px"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="auto"
+    class="demo-ruleForm"
+  >
+    <div>
+      <h1>Login</h1>
+    </div>
+    <el-form-item prop="username">
+      <el-input
+        :prefix-icon="User"
+        v-model="ruleForm.username"
+        placeholder="Username"
+      />
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input
+        :prefix-icon="Lock"
+        v-model="ruleForm.password"
+        type="password"
+        autocomplete="off"
+        placeholder="Password"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button v-if="!isLoading" @click="submitForm(ruleFormRef)">
+        LOGIN
+      </el-button>
+      <el-button v-else :icon="Loading" loading />
+    </el-form-item>
+  </el-form>
+</template>
+
+<style scoped>
+h1 {
+  color: white;
+}
+
+.demo-ruleForm {
+  width: 100%;
+  margin-top: -200px;
+  margin-left: -20px;
+}
+
+:deep(.el-input__wrapper) {
+  background-color: transparent;
+}
+
+:deep(.el-input__inner) {
+  color: white;
+}
+
+:deep(.el-button) {
+  width: 100%;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import LoginForm from "../components/LoginForm.vue";
+</script>
+
+<template>
+  <div class="form-container">
+    <LoginForm />
+    <p>
+      Dont have an account?
+      <router-link to="/register">Register here</router-link>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.form-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+p {
+  color: white;
+  font-size: 14px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  color: rgb(167, 187, 80);
+}
+</style>
