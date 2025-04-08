e27f0380f58013a07aa0b35449a8e14d1acd7f5c
PLUS\James.Agang <james.agang@tbu.net>
2025-04-08 15:06:45 +0800

    CourseTypoError/BUG271-Modified the typographical error.

--- src/constants/courses.ts
+++ src/constants/courses.ts
@@ -3,7 +3,7 @@
 export const courseOptions = [
   "Bachelor of Science in Computer Science",
   "Bachelor of Science in Information Technology",
-  "Bachelor of Science in Tourism Management",
+  "Bachelor of Science in Tourism",
   "Bachelor of Science in Hotel and Restaurant Management",
   "Bachelor of Science in Nursing",
 ];
